25d2155a4b885a7cf07bc13b48f9befd4c099fe8
imFernandes23 <eduardosage2302@gmail.com>
2025-08-31 17:33:02 -0300

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'gestao', pathMatch: 'full'},
-  { path: 'gerenciamento', loadComponent: () => import('./pages/gerenciamento/gerenciamento.component/gerenciamento.component').then(m => m.GerenciamentoComponent) },
-  { path: 'gerenciamento/:schemaId/evento/:fileName', loadComponent: () => import('./pages/gerenciamento/evento-detail.component/evento-detail.component').then(m => m.EventoDetailComponent) },
-  { path: 'gestao', loadComponent: () => import('./pages/gestao/gestao.component/gestao.component').then(m => m.GestaoComponent) },
-  { path: 'gestao/novo/:fileName',  loadComponent: () => import('./pages/gestao/novo-evento-winzard.component/novo-evento-winzard.component').then(m => m.NovoEventoWinzardComponent) }
-];
